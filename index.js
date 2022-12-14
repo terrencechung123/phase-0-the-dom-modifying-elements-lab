2ffd8bfd6443d16c971e83fb593b628cf6b1c12a
Terrence Chung <terrencechung35@gmail.com>
2022-12-13 17:41:19 -0800

    commit tree 4e2492eb5297c885b76f0973d84b1856d6cb13d9

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+const main = document.getElementById("main");
+main.remove();
+const newHeader = document.createElement("h1");
+
+newHeader.setAttribute("id", "victory");
+newHeader.textContent = "Emily is the champion";
