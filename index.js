81b24809482ff73b2b3a18bcfbbe923add017078
Terrence Chung <terrencechung35@gmail.com>
2023-01-19 15:05:19 -0800

    lab

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Write your code here!
+const main = document.querySelector('main');
+main.remove();
+
+const newHeader = document.createElement('h1');
+
+//the 'newHeader' variable that points to the <h1> node has an id of 'victory'
+
+newHeader.setAttribute("id","victory");
+
+newHeader.textContent = 'Terrence' + 'is the champion';
